34d836c63666d6c01cb0936176de1a032291530b
foobarberis <foobarberis@gmail.com>
2025-04-09 11:41:09 +0200

    Remove custom binding.

--- init.lua
+++ init.lua
@@ -5,9 +5,6 @@ vim.opt.relativenumber = true
 -- Highlight the current line
 vim.opt.cursorline = true
 
--- Remap exiting terminal mode to <C-t>
-vim.api.nvim_set_keymap('t', '<C-t>', [[<C-\><C-n>]], { noremap = true, silent = true })
-
 -- Use a single global status line
 vim.opt.laststatus = 3
 
