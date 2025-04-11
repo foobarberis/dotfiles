6a8f9d713eb791eb012593141ea5ec41c4171cb0
foobarberis <foobarberis@gmail.com>
2025-04-11 22:30:58 +0200

    Cleanup deploy.sh.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,17 @@
 
 mkdir -p ${HOME}/.config/alacritty
 
+# On macOS .bash_profile is read instead of .bashrc
+cp .bashrc ~/.bash_profile
+
+# Deploy dotfiles
 cp .bashrc .bash_aliases .tmux.conf .vimrc ${HOME}
+
+# Deploy configuration for Alacritty Linux / macOS
 cp alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
+
+# Deploy configuration for Alacritty on Windows
 # cp alacritty.toml /mnt/c/Users/16018659/AppData/Roaming/alacritty 
 
+# Deploy personal scripts
 chmod +x ./bin/* && sudo cp ./bin/* /usr/local/bin
