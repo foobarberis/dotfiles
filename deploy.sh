500e6cab988ef9751b337f8767fa5e4f880cd0f4
foobarberis <foobarberis@gmail.com>
2025-04-18 16:05:12 +0200

    main: Create ssh configuration.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p ${HOME}/.config/alacritty
+mkdir -p ${HOME}/.ssh
 
 # On macOS .bash_profile is read instead of .bashrc
 cp .bashrc ${HOME}/.bash_profile
@@ -16,3 +17,6 @@ cp alacritty.toml ${HOME}/.config/alacritty/alacritty.toml
 
 # Deploy personal scripts
 chmod +x ./bin/* && sudo cp ./bin/* /usr/local/bin
+
+# Deploy SSH configuration
+cp ssh_config ${HOME}/.ssh/config
