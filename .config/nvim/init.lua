7b35e2fb180ec70aeb3d0b9aa0f4fb19968a25fb
Matthieu Barberis <matthieu.barberis@biomerieux.com>
2025-05-01 00:10:31 +0200

    Cleanup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,28 +1,53 @@
+-- Load lazy configuration.
 require("config.lazy")
 
+-- Set leader key.
 vim.g.mapleader = " "
 
-vim.opt.syntax = "on"
+-- Enable mouse in all modes.
 vim.opt.mouse = "a"
-vim.opt.ruler = true
-vim.opt.incsearch = true
-vim.opt.laststatus = 2
-vim.opt.autoindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
 
--- Threshold for reporting number of lines changed
+-- Show cursor position.
+vim.opt.ruler = true
+
+-- Enable incremental search.
+vim.opt.incsearch = true
+
+-- Always display status line.
+vim.opt.laststatus = 2
+
+-- Auto-indent new lines.
+vim.opt.autoindent = true
+
+-- Set tab size.
+vim.opt.tabstop = 2
+
+-- Set number of spaces to use for indentation.
+vim.opt.shiftwidth = 2
+
+-- Convert tabs to spaces.
+vim.opt.expandtab = true
+
+-- Show line numbers.
+vim.opt.number = true
+
+-- Highlight current line.
+vim.opt.cursorline = true
+
+-- Always report changes (no threshold).
 vim.opt.report = 0
 
--- Whitespace characters visibility
+-- Show whitespace characters.
+-- Enable with :set list
 vim.opt.listchars = { eol = "⏎", tab = "␉·", trail = "␠", nbsp = "⎵" }
-vim.opt.list = false
 
--- Key mappings
+-- Map leader+n to a new note script.
 vim.api.nvim_set_keymap("n", "<leader>n", ":r !sh ~/.local/bin/new-note.sh<CR>", { noremap = true, silent = true })
 
+-- Remove "disable mouse" from PopUp menu.
 vim.cmd("aunmenu PopUp.How-to\\ disable\\ mouse")
 vim.cmd("aunmenu PopUp.-1-")
+
+-- Set "quiet" colorscheme.
+vim.cmd("colorscheme quiet")
+
