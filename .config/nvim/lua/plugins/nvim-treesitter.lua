0df072148ec3c425f6ab6234f8d09d7a190dc776
foobarberis <foobarberis@gmail.com>
2025-05-02 17:11:26 +0200

    main: Try new tree-sitter configuration

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,8 +1,18 @@
 local M = {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+    build = ":TSUpdate",
+    config = function()
+        local configs = require("nvim-treesitter.configs")
+
+        configs.setup({
+            ensure_installed = { "lua", "vim", "javascript", "html", "css", "yaml", "c", "cpp", "xml", "bash", "dockerfile"}, 
+            sync_install = false,
+            highlight = { enable = true },
+            indent = { enable = true },
+            auto_install = false,
+        })
+    end
 }
 
-return { M }
+return M
+
